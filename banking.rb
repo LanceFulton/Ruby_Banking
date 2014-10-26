0bef05cafa1f386c6f6e6a050fed82268148e435
LanceFulton <lsfaccounts@gmail.com>
2014-10-26 01:26:41 -0500

    added accounts hash to Bank. fixeddeposit method

--- banking.rb
+++ banking.rb
@@ -6,14 +6,21 @@
 # Classes
 
 Bank = Class.new do
+	attr_accessor :accounts
 	def initialize(bankname)
 		@bankname = bankname
+		@accounts = {}
 		puts "#{@bankname} bank was just created."
 	end
 	def open_account(customer)
 		@customer = customer
-		@customer_bal = 0
+		@accounts[customer.name] = 0
 		puts "#{@customer.name}, thanks for opening an account at #{@bankname}!"
+	end
+	def deposit (customer, deposit)
+		@accounts[customer.name] += deposit
+		customer.cash -= deposit
+		puts "#{customer.name} deposited $#{deposit} to #{@bankname}. #{customer.name} has $#{customer.cash}. #{customer.name}'s account has $" + @accounts[customer.name].to_s + "."
 	end
 end
 
--- testcode.rb
+++ testcode.rb
@@ -1,2 +1,10 @@
 require_relative 'banking.rb'
 
+chase = Bank.new("JP Morgan Chase")
+wells_fargo = Bank.new("Wells Fargo")
+me = Person.new("Shehzan", 500)
+friend1 = Person.new("John", 1000)
+chase.open_account(me)
+chase.open_account(friend1)
+wells_fargo.open_account(me)
+wells_fargo.open_account(friend1)
